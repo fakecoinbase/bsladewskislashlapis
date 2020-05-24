b4eead7dca26f58813ad20c0f10b61c77f6ebb96
Benjamin Sladewski <benjaminsladewski@terra.local>
2020-05-24 16:14:53 -0400

    Added a mock coinbase client.
    Added a mock coinbase client that returns historical data when
    the GetSpotPrice request made.

--- coinbase/coinbase.go
+++ coinbase/coinbase.go
@@ -2,17 +2,138 @@
 package coinbase
 
 import (
+	"encoding/csv"
 	"encoding/json"
+	"errors"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
+	"os"
+	"sort"
 	"strconv"
 	"time"
 )
 
-// client is used to execute requests to the coinbase API.
-var client = http.Client{
-	Timeout: 30 * time.Second,
+var (
+	// ErrEndOfMockData indicates that the end of the historical data used to
+	// mock the coinbase API has been reached.
+	ErrEndOfMockData = errors.New("end of mock data")
+)
+
+// Client provides functions for interacting with the coinbase API.
+type Client interface {
+	// GetSpotPrice retrieves the exchange rate between one BTC and USD.
+	GetSpotPrice() (float64, error)
+}
+
+// A defaultClient is used to interact with the coinbase API.
+type defaultClient struct {
+	client http.Client
+}
+
+// NewClient retrieves a client that can be used to interact with the coinbase
+// API.
+func NewClient() Client {
+
+	return &defaultClient{
+		client: http.Client{Timeout: 15 * time.Second},
+	}
+
+}
+
+// A mockClient mocks interactions with the coinbase API.
+type mockClient struct {
+	index      int
+	spotPrices []float64
+}
+
+// NewMockClient retrieves a client that can be used to mock interactions with
+// the coinbaes API.
+func NewMockClient() (Client, error) {
+
+	// open file containing mock data
+	mockDataFile, err := os.Open("mock_data.csv")
+	if err != nil {
+		return nil, fmt.Errorf("open mock data file, err: %v", err)
+	}
+	defer mockDataFile.Close()
+
+	// read mock data as comma separated values
+	reader := csv.NewReader(mockDataFile)
+
+	// define type for storing close prices associated with a specific timestamp
+	type priceDatum struct {
+		Timestamp time.Time
+		Price     float64
+	}
+
+	priceData := []priceDatum{}
+
+	// headers indicates that the record we are currently looking at is a row of
+	// headers and not data
+	headers := true
+
+	// loop until we are finished reading the csv data
+	for {
+
+		// read the next row
+		record, err := reader.Read()
+
+		// if we are looking at headers, skip the current row and mark that
+		// headers have already been read
+		if headers {
+			headers = false
+			continue
+		}
+
+		if err == io.EOF {
+			// if we are at the end of the file, the file has been successfully
+			// read
+			break
+		} else if err != nil {
+			// if any error other than an EOF is encountered, return the error
+			// as we were unable to parse the csv file
+			return nil, fmt.Errorf("reading mock data, err: %v", err)
+		}
+
+		// parse the timestamp from the csv data; as the data starts with the
+		// newest record we will need to sort by timestamp ascending when the
+		// file has been read
+		timestamp, err := time.Parse("2006-01-02 03-PM", record[0])
+		if err != nil {
+			return nil, fmt.Errorf("parsing timestamp, err: %v", err)
+		}
+
+		// parse the close price from csv data; the close price will represent
+		// the spot price for this index
+		price, err := strconv.ParseFloat(record[5], 64)
+		if err != nil {
+			return nil, fmt.Errorf("parsing price, err: %v", err)
+		}
+
+		// add the timestamp and close price to price data
+		priceData = append(priceData, priceDatum{timestamp, price})
+
+	}
+
+	// sort price data by timestamp ascending
+	sort.Slice(priceData, func(i, j int) bool {
+		return priceData[i].Timestamp.Before(priceData[j].Timestamp)
+	})
+
+	// build ordered list of price data
+	spotPrices := []float64{}
+	for _, priceDatum := range priceData {
+		spotPrices = append(spotPrices, priceDatum.Price)
+	}
+
+	// return the mock client starting at index zero of historical price data
+	return &mockClient{
+		index:      0,
+		spotPrices: spotPrices,
+	}, nil
+
 }
 
 // spotPriceResponse is used to read the exchange rate returned by the get spot
@@ -21,8 +142,7 @@ type spotPriceResponse struct {
 	Amount string `json:"amount"`
 }
 
-// GetSpotPrice retrieves the exchange rate between one BTC and USD.
-func GetSpotPrice() (float64, error) {
+func (d *defaultClient) GetSpotPrice() (float64, error) {
 
 	// create a new GET request to coinbase spot price endpoint
 	req, err := http.NewRequest(
@@ -34,7 +154,7 @@ func GetSpotPrice() (float64, error) {
 	}
 
 	// execute the request
-	resp, err := client.Do(req)
+	resp, err := d.client.Do(req)
 	if err != nil {
 		return 0.0, err
 	}
@@ -68,5 +188,20 @@ func GetSpotPrice() (float64, error) {
 	}
 
 	return rate, nil
+
+}
+
+func (m *mockClient) GetSpotPrice() (float64, error) {
+
+	// retrieve the next item of mock data and increment current index into mock
+	// data
+	if m.index < len(m.spotPrices) {
+		spotPrice := m.spotPrices[m.index]
+		m.index++
+		return spotPrice, nil
+	}
+
+	// return an error indicating that we have reached the end of mock data
+	return 0.0, ErrEndOfMockData
 
 }
--- coinbase/coinbase_test.go
+++ coinbase/coinbase_test.go
@@ -11,8 +11,11 @@ import (
 // TestGetSpotPrice tests retrieving a spot price from the coinbase API.
 func TestGetSpotPrice(t *testing.T) {
 
+	// construct the coinbase client
+	client := coinbase.NewClient()
+
 	// retrieve spot price
-	rate, err := coinbase.GetSpotPrice()
+	rate, err := client.GetSpotPrice()
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -23,7 +26,41 @@ func TestGetSpotPrice(t *testing.T) {
 		t.Fatalf("expected rate greater than zero, got %.2f", rate)
 	}
 
-	// log the retrieve rate
+	// log the retrieved rate
 	t.Log(rate)
+
+}
+
+// TestGetMockSpotPrice test loading and retrieving historical spot prices.
+func TestGetMockSpotPrice(t *testing.T) {
+
+	// construct the coinbase mock client
+	client, err := coinbase.NewMockClient()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for {
+
+		rate, err := client.GetSpotPrice()
+		if err == coinbase.ErrEndOfMockData {
+			break
+		}
+
+		// assert next item of mock data was read without error
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		// assert spot price is greater than zero (if this check fails because the
+		// exchange IS zero we've got bigger things to worry about)
+		if rate <= 0.0 {
+			t.Fatalf("expected rate greater than zero, got %.2f", rate)
+		}
+
+		// log the retrieved rate
+		t.Log(rate)
+
+	}
 
 }
